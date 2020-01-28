1fea979976b349a7650a40d2768ea16a81556403
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-01-28 23:02:25 +0300

    wip

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,29 +95,20 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
+    // this.setState({ loading: true });
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Alex',
-        address: {
-          street: 'Park Avenu 7',
-          zipCode: '77558',
-          country: 'USA'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
+    const queryParams = [];
 
-    axios
-      .post('/orders.json', order)
-      .then(this.setState({ loading: false, purchasing: false }))
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    for(let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    }
+
+    const queryString = queryParams.join('&')
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?'+ queryString
+    })
   };
 
   render() {
