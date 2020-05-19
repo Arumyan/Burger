670c7fcb2f87de390ab2df7c94804f327a3c2bfe
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-05-19 18:51:30 +0300

    wip

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import classes from './Burger.module.scss'
+import React from 'react';
+import classes from './Burger.module.scss';
 
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+  const transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])]
+      .map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      })
+      .reduce((arr, el) => {
+        return arr.concat(el);
+      }, []);
+  });
+
+  console.log(transformIngredients);
   return (
     <div className={classes.Burger}>
-      <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="meat"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type='bread-top' />
+      {transformIngredients}
+      <BurgerIngredient type='bread-bottom' />
     </div>
-  )
-}
+  );
+};
 
-export default Burger
+export default Burger;
