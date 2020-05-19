1b50e8da755588536627d29c11d103e506512986
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-05-19 22:24:14 +0300

    create controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import classes from './BuildControl.module.scss'
+import React from 'react';
+import classes from './BuildControl.module.scss';
 
 const BuildControl = (props) => {
-  return ( 
-    <div>
-      <div>{props.label}</div>
-      <button>Less</button>
-      <button>More</button>
+  return (
+    <div className={classes.BuildControl}>
+      <div className={classes.Label}>{props.label}</div>
+      <button
+        className={classes.Less}
+        onClick={props.removed}
+        disabled={props.disabled}
+      >
+        Less
+      </button>
+      <button className={classes.More} onClick={props.added}>
+        More
+      </button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default BuildControl;
