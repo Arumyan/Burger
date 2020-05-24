68cffd7d6b3dd356f82bdfaa702afceefefc76bd
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-05-24 09:49:41 +0300

    markup

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Toolbar.module.scss';
+
+const Toolbar = (props) => {
+  return (
+    <header>
+      <div>MENU</div>
+      <div>LOGO</div>
+      <nav>...</nav>
+    </header>
+  );
+};
+
+export default Toolbar;
