4d6f8fa9018e453e2957de8ccecc9d59c9b4d928
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-05-28 11:41:19 +0300

    add routing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 0,
-      meat: 0,
-      cheese: 0,
-      bacon: 0,
-    },
+    ingredients: null,
+    price: 0,
   };
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
-    const ingredients = {}
-    for(let param of query.entries()) {
-      ingredients[param[0]] = +param[1]
+    const ingredients = {};
+    let price = 0;
+
+    for (let param of query.entries()) {
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ingredients: ingredients})
+
+    this.setState({ ingredients: ingredients, totalPrice: price });
   }
 
   checkoutCancelledHandler = () => {
-    this.props.history.goBack()
-  }
+    this.props.history.goBack();
+  };
 
   checkoutContinuedHandler = () => {
-    this.props.history.replace('/checkout/contact-data')
-  }
+    this.props.history.replace('/checkout/contact-data');
+  };
 
   render() {
     return (
@@ -35,6 +39,16 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          render={(props) => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+              {...props}
+            />
+          )}
         />
       </div>
     );
